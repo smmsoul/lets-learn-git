5ff4a2e4b992a89cf5ec2e35f948cd3e5dea8b26
Sumesh Kumar <officialsumesh07@gmail.com>
2023-03-31 16:33:18 +0530

    initial commit

---
+++ sum.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+
+let result = parseFloat(num1) + parseFloat(num2);
+
+console.log("The sum of " + num1 + " and " + num2 + " is " + result);
